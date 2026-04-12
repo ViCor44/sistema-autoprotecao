ae5ba92cac3df189ca4ed674fb04e05c4646827f
ViCor44 <victor.a.correia@gmail.com>
2026-04-12 23:29:15 +0100

    Create PHP configuration constants in Config.php

---
+++ config/Config.php
@@ -0,0 +1,10 @@
+<?php
+// PHP configuration constants
+
+// Database configuration
+const DB_HOST = 'localhost';
+const DB_NAME = 'your_database_name';
+const DB_USER = 'your_username';
+const DB_PASS = 'your_password';
+
+// Other constants can be added here...
